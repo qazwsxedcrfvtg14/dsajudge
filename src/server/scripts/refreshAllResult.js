9e338e4d5c1babb68566c6e9d84d6529d982a570
Meteor <bobogei81123@gmail.com>
2016-10-14 01:31:16 +0800

    Show progress in refreshResult

--- src/server/scripts/refreshAllResult.js
+++ src/server/scripts/refreshAllResult.js
@@ -5,13 +5,21 @@ import Submission from '/model/submission';
 
 const main = async () => {
     let ls = await Submission.find();
+    let len = ls.length;
+    let cnt = 0;
     for (let sub of ls) {
         await updateProblemResult(sub);
+        cnt++;
+        if (cnt % 100 == 0) console.log(`Updated ${cnt}/${len}`);
     }
 
     ls = await HomeworkResult.find();
+    len = ls.length;
+    cnt = 0;
     for (let res of ls) {
         await updateHomeworkResult(res);
+        cnt++;
+        if (cnt % 100 == 0) console.log(`Updated ${cnt}/${len}`);
     }
     console.log('done');
 };
