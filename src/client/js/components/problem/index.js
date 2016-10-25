418bc09a4d632c0c4bfbc3734d16c8440aecc385
Meteor <bobogei81123@gmail.com>
2016-10-25 23:12:57 +0800

    fix problem view

--- src/client/js/components/problem/index.js
+++ src/client/js/components/problem/index.js
@@ -4,6 +4,8 @@ import marked from 'marked';
 import sleep from 'sleep-promise';
 import probUtils from '/mixins/probUtils';
 import './index.css';
+import randomColor from 'randomcolor';
+import store, {getUser} from '/store';
 
 const renderer = new marked.Renderer();
 renderer.heading = (text, level) => {
@@ -19,16 +21,40 @@ export default Vue.extend({
         };
     },
     template: html,
-    ready() {
-        (async () => {
-            this.problem = (await this.$http.get(`/problem/${this.$route.params.id}`)).data; 
-            await sleep(500);
+    async ready() {
+        this.problem = (await this.$http.get(`/problem/${this.$route.params.id}`)).data; 
+        Vue.nextTick( () => {
             MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-        })();
+            this.drawBar();
+        } );
+    },
+    methods: {
+        drawBar() {
+            if (!this.problem) return;
+            const wrapper = $('#testgroup-bar-wrapper');
+            const tc = this.problem.testdata.groups.length;
+            const totp = this.problem.testdata.points;
+            const totl = wrapper.width();
+            const rc = randomColor({ count: tc, format: 'rgb', luminosity: 'bright', });
+            for (let [i, g] of this.problem.testdata.groups.entries()) {
+                const div = $('<div>');
+                div.text(`#${i} (${g.points})`);
+                div.addClass('testgroup-bar');
+                div.width(g.points / totp * totl);
+                div.css('background-color', 'rgba' + rc[i].substring(3, rc[i].length-1) + ', 0.5)');
+                wrapper.append(div);
+            }
+        },
     },
     filters: {
         marked(x) {
             return marked(x, {renderer});
+        },
+    },
+    store,
+    vuex: {
+        getters: {
+            user: getUser,
         },
     }
 });
