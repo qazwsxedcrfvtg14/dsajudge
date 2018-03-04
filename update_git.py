b8803e605fc9c98b8e1182054ab2d9d0c7c9fdf7
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-03-04 13:24:35 +0800

    add git server

---
+++ update_git.py
@@ -0,0 +1,35 @@
+import os
+import time
+while True:
+    pubs=os.listdir("gitosis-admin/keydir")
+    f=open("gitosis-admin/gitosis.conf","w")
+    f.write("[gitosis]\n\n")
+    f.write("[group gitosis-admin]\n")
+    f.write("members = mvnl@mvnl-Saab\n")
+    f.write("writable = gitosis-admin init\n\n")
+    for pub in pubs:
+        if(pub.endswith(".pub")):
+            name=pub[:-4]
+        else:
+            continue
+        f.write("[group "+name+"]\n")
+        f.write("members = "+name+"\n")
+        f.write("writable = "+name+"\n\n")
+    f.close()
+    os.system("git -C gitosis-admin add .")
+    os.system("git -C gitosis-admin commit -m update")
+    os.system("git -C gitosis-admin push")
+    time.sleep(15)
+
+"""
+[gitosis]
+
+[group gitosis-admin]
+members = mvnl@mvnl-Saab
+writable = gitosis-admin
+
+[group b05902086]
+members = b05902086
+writable = b05902086
+
+"""
