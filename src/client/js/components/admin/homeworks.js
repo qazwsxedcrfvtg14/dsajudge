4f41c45c14f7fce7f28c8c5f14793fa8ee0f36e3
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-03-05 01:24:23 +0800

    try to fix homeworks.js

--- src/client/js/components/admin/homeworks.js
+++ src/client/js/components/admin/homeworks.js
@@ -1,16 +1,77 @@
 import Vue from 'vue';
-import template from './problems.pug';
+import template from '../home/index.pug';
 
 export default Vue.extend({
-    template,
     data() {
         return { 
-            problems: [],
+            homeworks: [ ],
         };
     },
+    template: html,
     ready() {
-        (async () => {
-            this.problems = (await this.$http.get(`/admin/problem/`)).data; 
-        })();
+        this.fetchHomeworks();
+    },
+    store,
+    vuex: {
+        getters: {
+            user: getUser,
+        }
+    },
+    filters: {
+        marked(x) {
+            return marked(x);
+        },
+    },
+    methods: {
+        async fetchHomeworks() {
+            if (!this.user) {
+                this.homeworks = [];
+                return;
+            }
+            this.homeworks = (await this.$http.get('/homework/')).data; 
+        },
+        async newHomework() {
+            let id;
+            try {
+                id = (await this.$http.put('/admin/homework/')).data.id;
+            } catch(e) {
+                if (e.body) toastr.error(e.body);
+                else toastr.error(e);
+            }
+            this.$route.router.go({
+                name: 'admin.homework',
+                params: {
+                    id,
+                }
+            });
+        },
+        getRankStr(hw) {
+            const {rank} = hw;
+            if (!rank) return '50%';
+            return `${rank}`;
+        },
+        getRankClass(hw) {
+            const {rank, totUsers} = hw;
+            if (!rank) return 'rank-under-50';
+            const pr = (rank-1) / totUsers;
+            if (pr < 1.0 / 24) return 'rank-gold';
+            if (pr < 1.0 / 8) return 'rank-silver';
+            if (pr < 1.0 / 4) return 'rank-bronze';
+        },
+        getRankImage(hw) {
+            const {rank, totUsers} = hw;
+            if (!rank) return '';
+            const pr = (rank-1) / totUsers;
+            let s;
+            if (pr < 1.0 / 24) s = 'gold';
+            else if (pr < 1.0 / 8) s = 'silver';
+            else if (pr < 1.0 / 4) s = 'bronze';
+            return s ? `images/medal_${s}.jpg` : null; 
+        },
+    },
+    watch: {
+        user() {
+            this.fetchHomeworks();
+        }
     },
 });
