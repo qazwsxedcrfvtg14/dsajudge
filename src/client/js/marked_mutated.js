c549d645b13f0f6508f63c2c498cd49278d1dfab
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-03-04 17:45:03 +0800

    fix something that the server down

--- src/client/js/marked_mutated.js
+++ src/client/js/marked_mutated.js
@@ -2,7 +2,7 @@ import marked from 'marked';
 
 // This suppresses the transformation from "_text_" to "<em>text</em>", which
 // often incorrectly transforms mathjax content, such as $a_1, a_2$.
-
+/*
 marked.prototype.constructor.Parser.prototype.parse = function (src) {
     this.inline = new marked.InlineLexer(src.links, this.options, this.renderer);
     this.inline.rules.em = { exec: $.noop };
@@ -15,5 +15,5 @@ marked.prototype.constructor.Parser.prototype.parse = function (src) {
 
     return out;
 };
-
+*/
 export default marked;
