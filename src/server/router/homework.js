0a7d07fa3620f3861c75bcba0905d838965ebee6
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-03-07 01:32:50 +0800

    Upload PDF

--- src/server/router/homework.js
+++ src/server/router/homework.js
@@ -74,8 +74,44 @@ router.get('/:id', wrap(async (req, res) => {
     res.send("not construct");
 }));
 
-router.get('/:id/submit', wrap(async (req, res) => {
-
+router.get('/:id/submit', requireLogin, wrap(async (req, res) => {
+    try{
+        const hid=req.params.id;
+        const hwDir=path.join(config.dirs.homeworks, hid);
+        try {
+            await fs.stat(hwDir);
+        } catch(e) {
+            await fs.mkdir(hwDir);
+        }
+        const userDir=path.join(hwDir,req.user.meta.id);
+        try {
+            await fs.stat(userDir);
+        } catch(e) {
+            await fs.mkdir(userDir);
+        }
+        const file_name=moment(Date.now()).tz('Asia/Taipei').format('YYYY/MM/DD HH:mm:ss')+".pdf";
+        await fs.writeFile(
+            path.join(userDir, file_name ),
+            req.body.file,
+        );
+        if(!req.user.homeworks)req.user.homeworks=[];
+        const homeworks=req.user.homeworks;
+        let filter_res = _.filter(homeworks,_.conforms({ homework_id : id => id==hid }));
+        let res ;
+        if (filter_res === undefined || filter_res.length == 0){
+            res={
+                homework_id: hid,
+            };
+            req.user.homeworks.push(res);
+        }else{
+            res = filter_res[0];
+        }
+        res.file_name=file_name;
+        await req.user.save();
+        res.send(`Upload successfully.`);
+    } catch(e) {
+        return res.status(500).send(`Something bad happened... Your file may not be saved.`);
+    }
 }));
 
 export default router;
