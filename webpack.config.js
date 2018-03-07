f0cdf04e455038e82a29f26d5f8738e77efb5b78
owo <owo@owo.com>
2018-03-07 17:21:14 +0800

    babel_support

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const cssnano = require('cssnano')({autoprefixer: false});
 const webpack = require('webpack');
 const path = require('path');
 module.exports = {
+	 entry: ["babel-polyfill", "./src/client/js/app.js"],
 	resolve:{
 		alias:{
 			vue: 'vue/dist/vue.js'
@@ -23,7 +24,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'eslint-loader',
+                loader: 'babel-loader',
             },
 			{
 				test: /\.css$/,
@@ -37,35 +38,6 @@ module.exports = {
 				test: /\.pug$/,
 				use: ['raw-loader', 'pug-html-loader']
 			}
-        ],
-        loaders: [
-            {
-                test: /\.js$/, 
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['latest'],
-                    plugins: [
-                        'transform-es2015-modules-commonjs',
-                        'transform-strict-mode',
-                        ['babel-root-slash-import', {
-                            rootPathSuffix: path.join(CONFIG.src.client.base, 'js'),
-                        }],
-                    ]
-                }
-            },
-            {
-                test: /\.css$/, 
-                loader: 'style!css!postcss',
-            },
-            {
-                test: /\.pug$/, 
-                loaders: ['html', 'pug-html'],
-            },
-            {
-                test: /\.json$/, 
-                loaders: ['json-loader'],
-            },
         ],
     },
     stats: {
