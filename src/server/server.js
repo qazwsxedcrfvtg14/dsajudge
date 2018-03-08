2efa99ab0d6c4a4bbb350734a8f75d8566ed779a
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-03-09 02:33:15 +0800

    try to start judge

--- src/server/server.js
+++ src/server/server.js
@@ -8,15 +8,33 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import expressSession from 'express-session';
 import mongoose from 'mongoose';
-mongoose.connect(config.mongo.url);
-mongoose.Promise = Promise;
 import autoIncrement from 'mongoose-auto-increment';
-const MongoStore = require('connect-mongo')(expressSession); 
 import logger from './logger';
-const app = express();
 import setRouter from './router';
 import judger from '/judger';
 
+mongoose.connect(config.mongo.url);
+mongoose.Promise = Promise;
+const MongoStore = require('connect-mongo')(expressSession); 
+const app = express();
+app.use('/static',express.static('static'));
+app.use(express.static('static'));
+app.use(cookieParser());
+app.use(expressSession({
+    secret: config.secret,
+    maxAge: 1000 * 3600 * 1000,
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        url: config.mongo.url,
+        touchAfter: 3600,
+    }),
+}));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({limit: '20mb',extended: true}));
+auth(app);
+setRouter(app);
     
 if (cluster.isMaster) {
     logger.info(`Master ${process.pid} is running`);
@@ -34,23 +52,5 @@ if (cluster.isMaster) {
 }
 else{
     logger.info(`Worker ${process.pid} started`);
-    app.use('/static',express.static('static'));
-    app.use(express.static('static'));
-    app.use(cookieParser());
-    app.use(expressSession({
-        secret: config.secret,
-        maxAge: 1000 * 3600 * 1000,
-        secure: false,
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({
-            url: config.mongo.url,
-            touchAfter: 3600,
-        }),
-    }));
-    app.use(bodyParser.json({limit: '20mb'}));
-    app.use(bodyParser.urlencoded({limit: '20mb',extended: true}));
-    auth(app);
-    setRouter(app);
     app.listen(config.port, '127.0.0.1', () => logger.info(`Server start @ ${config.port}`));
 }
