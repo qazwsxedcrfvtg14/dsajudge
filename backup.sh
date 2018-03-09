57cbda75fe03c7d62357bae0424e6b7ba01b4cbf
owo <owo@owo.com>
2018-03-09 15:46:23 +0800

    update backup

--- backup.sh
+++ backup.sh
@@ -1,8 +1,16 @@
+#/bin/bash
+#cd "$( dirname "${BASH_SOURCE[0]}" )"
+#DIR="$(cd -P "$(dirname "$0")" && pwd)"
+cd -P "$(dirname "$0")"
 backup_dir=../backup
 DATE=`date +%Y%m%d-%H%M%S`
 root_dir=$backup_dir/$DATE
 submissions_dir=./submissions
+homeworks_dir=./homeworks
+git_dir=/home/git/repositories/
 mkdir -p $root_dir
 mongodump --archive=$root_dir/dsajudge.${DATE}.gz --gzip --db dsajudge
 tar -zcf $root_dir/submissions.tar.gz $submissions_dir
-
+tar -zcf $root_dir/homeworks.tar.gz $homeworks_dir
+tar -zcf $root_dir/git.tar.gz $git_dir
+gdrive upload -r $root_dir
