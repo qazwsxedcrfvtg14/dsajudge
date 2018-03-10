8b0427c127be6a1643ff5c232510706779f10382
owo <owo@owo.com>
2018-03-10 17:01:53 +0800

    backup prob.md

--- backup.sh
+++ backup.sh
@@ -7,10 +7,12 @@ DATE=`date +%Y%m%d-%H%M%S`
 root_dir=$backup_dir/$DATE
 submissions_dir=./submissions
 homeworks_dir=./homeworks
-git_dir=/home/git/repositories/
+problems_dir=./problems
+git_dir=/home/git/repositories
 mkdir -p $root_dir
 mongodump --archive=$root_dir/dsajudge.${DATE}.gz --gzip --db dsajudge
 tar -zcf $root_dir/submissions.tar.gz $submissions_dir
 tar -zcf $root_dir/homeworks.tar.gz $homeworks_dir
 tar -zcf $root_dir/git.tar.gz $git_dir
+tar -zcf $root_dir/problems.tar.gz $problems_dir/*/prob.md
 gdrive upload -r $root_dir
