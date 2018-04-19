9cb93c787ff8c20a05a597b6d7c6575511154667
owo <owo@owo.com>
2018-04-19 11:28:35 +0800

    update_isolate

--- isolate/isolate.c
+++ isolate/isolate.c
@@ -57,7 +57,8 @@
  *
  *   - Error pipe for error messages produced by the proxy process and the early
  *     stages of the inside process (until exec()). Listened to by the keeper.
- *   - Status pipe for passing the exit status from the proxy to the keeper.
+ *   - Status pipe for passing the PID of the inside process and its exit status
+ *     from the proxy to the keeper.
  */
 
 #define TIMER_INTERVAL_US 100000
@@ -79,14 +80,16 @@ static int redir_stderr_to_stdout;
 static char *set_cwd;
 static int share_net;
 static int inherit_fds;
+static int default_dirs = 1;
 
 int cg_enable;
 int cg_memory_limit;
-int cg_timing;
+int cg_timing = 1;
 
 int box_id;
 static char box_dir[1024];
 static pid_t box_pid;
+static pid_t proxy_pid;
 
 uid_t box_uid;
 gid_t box_gid;
@@ -130,16 +133,21 @@ final_stats(struct rusage *rus)
 static void NONRET
 box_exit(int rc)
 {
-  if (box_pid > 0)
+  if (proxy_pid > 0)
     {
-      kill(-box_pid, SIGKILL);
-      kill(box_pid, SIGKILL);
+      if (box_pid > 0)
+	{
+	  kill(-box_pid, SIGKILL);
+	  kill(box_pid, SIGKILL);
+	}
+      kill(-proxy_pid, SIGKILL);
+      kill(proxy_pid, SIGKILL);
       meta_printf("killed:1\n");
 
       struct rusage rus;
       int p, stat;
       do
-	p = wait4(box_pid, &stat, 0, &rus);
+	p = wait4(proxy_pid, &stat, 0, &rus);
       while (p < 0 && errno == EINTR);
       if (p < 0)
 	fprintf(stderr, "UGH: Lost track of the process (%m)\n");
@@ -171,11 +179,16 @@ die(char *msg, ...)
   char buf[1024];
   int n = vsnprintf(buf, sizeof(buf), msg, args);
 
-  // If the child process is still running, show no mercy.
+  // If the child processes are still running, show no mercy.
   if (box_pid > 0)
     {
       kill(-box_pid, SIGKILL);
       kill(box_pid, SIGKILL);
+    }
+  if (proxy_pid > 0)
+    {
+      kill(-proxy_pid, SIGKILL);
+      kill(proxy_pid, SIGKILL);
     }
 
   if (write_errors_to_fd)
@@ -324,24 +337,28 @@ reset_signals(void)
 /*** The keeper process ***/
 
 #define PROC_BUF_SIZE 4096
-static void
+static int
 read_proc_file(char *buf, char *name, int *fdp)
 {
   int c;
 
-  if (!*fdp)
+  if (*fdp < 0)
     {
-      sprintf(buf, "/proc/%d/%s", (int) box_pid, name);
+      snprintf(buf, PROC_BUF_SIZE, "/proc/%d/%s", (int) box_pid, name);
       *fdp = open(buf, O_RDONLY);
       if (*fdp < 0)
-	die("open(%s): %m", buf);
+	return 0;	// This is OK, the process could have finished
     }
   lseek(*fdp, 0, SEEK_SET);
   if ((c = read(*fdp, buf, PROC_BUF_SIZE-1)) < 0)
-    die("read on /proc/$pid/%s: %m", name);
+    {
+      // Even this could fail if the process disappeared since open()
+      return 0;
+    }
   if (c >= PROC_BUF_SIZE-1)
     die("/proc/$pid/%s too long", name);
   buf[c] = 0;
+  return 1;
 }
 
 static int
@@ -356,7 +373,7 @@ get_wall_time_ms(void)
 static int
 get_run_time_ms(struct rusage *rus)
 {
-  if (cg_timing)
+  if (cg_enable && cg_timing)
     return cg_get_run_time_ms();
 
   if (rus)
@@ -366,11 +383,16 @@ get_run_time_ms(struct rusage *rus)
       return total.tv_sec*1000 + total.tv_usec/1000;
     }
 
+  // It might happen that we do not know the box_pid (see comments in find_box_pid())
+  if (!box_pid)
+    return 0;
+
   char buf[PROC_BUF_SIZE], *x;
   int utime, stime;
-  static int proc_stat_fd;
+  static int proc_stat_fd = -1;
 
-  read_proc_file(buf, "stat", &proc_stat_fd);
+  if (!read_proc_file(buf, "stat", &proc_stat_fd))
+    return 0;
   x = buf;
   while (*x && *x != ' ')
     x++;
@@ -449,16 +471,16 @@ box_keeper(void)
 	  check_timeout();
 	  timer_tick = 0;
 	}
-      p = wait4(box_pid, &stat, 0, &rus);
+      p = wait4(proxy_pid, &stat, 0, &rus);
       if (p < 0)
 	{
 	  if (errno == EINTR)
 	    continue;
 	  die("wait4: %m");
 	}
-      if (p != box_pid)
+      if (p != proxy_pid)
 	die("wait4: unknown pid %d exited!", p);
-      box_pid = 0;
+      proxy_pid = 0;
 
       // Check error pipe if there is an internal error passed from inside the box
       char interr[1024];
@@ -528,7 +550,7 @@ setup_root(void)
   if (mount("none", "root", "tmpfs", 0, "mode=755") < 0)
     die("Cannot mount root ramdisk: %m");
 
-  apply_dir_rules();
+  apply_dir_rules(default_dirs);
 
   if (chroot("root") < 0)
     die("Chroot failed: %m");
@@ -658,6 +680,8 @@ box_proxy(void *arg)
     }
 
   setup_orig_credentials();
+  if (write(status_pipes[1], &inside_pid, sizeof(inside_pid)) != sizeof(inside_pid))
+    die("Proxy write to pipe failed: %m");
 
   int stat;
   pid_t p = waitpid(inside_pid, &stat, 0);
@@ -727,14 +751,49 @@ cleanup(void)
 }
 
 static void
-setup_pipe(int *fds)
+setup_pipe(int *fds, int nonblocking)
 {
   if (pipe(fds) < 0)
     die("pipe: %m");
   for (int i=0; i<2; i++)
     if (fcntl(fds[i], F_SETFD, fcntl(fds[i], F_GETFD) | FD_CLOEXEC) < 0 ||
-        fcntl(fds[i], F_SETFL, fcntl(fds[i], F_GETFL) | O_NONBLOCK) < 0)
+        nonblocking && fcntl(fds[i], F_SETFL, fcntl(fds[i], F_GETFL) | O_NONBLOCK) < 0)
       die("fcntl on pipe: %m");
+}
+
+static void
+find_box_pid(void)
+{
+  /*
+   *  The box keeper process wants to poll status of the inside process,
+   *  so it needs to know the box_pid. However, it is not easy to obtain:
+   *  we got the PID from the proxy, but it is local to the PID namespace.
+   *  Instead, we ask /proc to enumerate the children of the proxy.
+   *
+   *  CAVEAT: The timing is tricky. We know that the inside process was
+   *  already started (passing the PID from the proxy to us guarantees it),
+   *  but it might already have exited and be reaped by the proxy. Therefore
+   *  it is correct if we fail to find anything.
+   */
+
+  char namebuf[256];
+  snprintf(namebuf, sizeof(namebuf), "/proc/%d/task/%d/children", (int) proxy_pid, (int) proxy_pid);
+  FILE *f = fopen(namebuf, "r");
+  if (!f)
+    return;
+
+  int child;
+  if (fscanf(f, "%d", &child) != 1)
+    {
+      fclose(f);
+      return;
+    }
+  box_pid = child;
+
+  if (fscanf(f, "%d", &child) == 1)
+    die("Error parsing %s: unexpected children found", namebuf);
+
+  fclose(f);
 }
 
 static void
@@ -749,19 +808,27 @@ run(char **argv)
   chowntree("box", box_uid, box_gid);
   cleanup_ownership = 1;
 
-  setup_pipe(error_pipes);
-  setup_pipe(status_pipes);
+  setup_pipe(error_pipes, 1);
+  setup_pipe(status_pipes, 0);
   setup_signals();
 
-  box_pid = clone(
+  proxy_pid = clone(
     box_proxy,			// Function to execute as the body of the new process
     argv,			// Pass our stack
     SIGCHLD | CLONE_NEWIPC | (share_net ? 0 : CLONE_NEWNET) | CLONE_NEWNS | CLONE_NEWPID,
     argv);			// Pass the arguments
-  if (box_pid < 0)
+  if (proxy_pid < 0)
     die("Cannot run proxy, clone failed: %m");
-  if (!box_pid)
+  if (!proxy_pid)
     die("Cannot run proxy, clone returned 0");
+
+  pid_t box_pid_inside_ns;
+  int n = read(status_pipes[0], &box_pid_inside_ns, sizeof(box_pid_inside_ns));
+  if (n != sizeof(box_pid_inside_ns))
+    die("Proxy failed before it passed box_pid: %m");
+  find_box_pid();
+  msg("Started proxy_pid=%d box_pid=%d box_pid_inside_ns=%d\n", (int) proxy_pid, (int) box_pid, (int) box_pid_inside_ns);
+
   box_keeper();
 }
 
@@ -793,6 +860,7 @@ Options:\n\
     --cg\t\tEnable use of control groups\n\
     --cg-mem=<size>\tLimit memory usage of the control group to <size> KB\n\
     --cg-timing\t\tTime limits affects total run time of the control group\n\
+\t\t\t(this is turned on by default, use --no-cg-timing to turn off)\n\
 -c, --chdir=<dir>\tChange directory to <dir> before executing the program\n\
 -d, --dir=<dir>\t\tMake a directory <dir> visible inside the sandbox\n\
     --dir=<in>=<out>\tMake a directory <out> outside visible as <in> inside\n\
@@ -803,6 +871,7 @@ Options:\n\
 \t\t\t\tmaybe\tSkip the rule if <out> does not exist\n\
 \t\t\t\tnoexec\tDo not allow execution of binaries\n\
 \t\t\t\trw\tAllow read-write access\n\
+-D, --no-default-dirs\tDo not add default directory rules\n\
 -f, --fsize=<size>\tMax size (in KB) of files that can be created\n\
 -E, --env=<var>\t\tInherit the environment variable <var> from the parent process\n\
 -E, --env=<var>=<val>\tSet the environment variable <var> to <val>; unset it if <var> is empty\n\
@@ -842,12 +911,13 @@ enum opt_code {
   OPT_CG,
   OPT_CG_MEM,
   OPT_CG_TIMING,
+  OPT_NO_CG_TIMING,
   OPT_SHARE_NET,
   OPT_INHERIT_FDS,
   OPT_STDERR_TO_STDOUT,
 };
 
-static const char short_opts[] = "b:c:d:eE:f:i:k:m:M:o:p::q:r:st:vw:x:";
+static const char short_opts[] = "b:c:d:DeE:f:i:k:m:M:o:p::q:r:st:vw:x:";
 
 static const struct option long_opts[] = {
   { "box-id",		1, NULL, 'b' },
@@ -857,6 +927,8 @@ static const struct option long_opts[] = {
   { "cg-timing",	0, NULL, OPT_CG_TIMING },
   { "cleanup",		0, NULL, OPT_CLEANUP },
   { "dir",		1, NULL, 'd' },
+  { "no-cg-timing",	0, NULL, OPT_NO_CG_TIMING },
+  { "no-default-dirs",  0, NULL, 'D' },
   { "fsize",		1, NULL, 'f' },
   { "env",		1, NULL, 'E' },
   { "extra-time",	1, NULL, 'x' },
@@ -886,6 +958,7 @@ int
 main(int argc, char **argv)
 {
   int c;
+  int require_cg = 0;
   char *sep;
   enum opt_code mode = 0;
 
@@ -907,6 +980,9 @@ main(int argc, char **argv)
 	if (!set_dir_action(optarg))
 	  usage("Invalid directory specified: %s\n", optarg);
 	break;
+      case 'D':
+        default_dirs = 0;
+        break;
       case 'e':
 	pass_environ = 1;
 	break;
@@ -975,9 +1051,15 @@ main(int argc, char **argv)
 	break;
       case OPT_CG_MEM:
 	cg_memory_limit = atoi(optarg);
+	require_cg = 1;
 	break;
       case OPT_CG_TIMING:
 	cg_timing = 1;
+	require_cg = 1;
+	break;
+      case OPT_NO_CG_TIMING:
+	cg_timing = 0;
+	require_cg = 1;
 	break;
       case OPT_SHARE_NET:
 	share_net = 1;
@@ -1000,6 +1082,9 @@ main(int argc, char **argv)
       show_version();
       return 0;
     }
+
+  if (require_cg && !cg_enable)
+    usage("Options related to control groups require --cg to be set.\n");
 
   if (geteuid())
     die("Must be started as root");
