2e5d1243037be812ffaf0e6bd129572d537aa346
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-09-20 16:13:46 +0800

    add useNewUrlParser

--- src/server/scripts/common.js
+++ src/server/scripts/common.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill';
 import config from '/config';
 import mongoose from 'mongoose';
-mongoose.connect(config.mongo.url);
+mongoose.connect(config.mongo.url, {useNewUrlParser: true});
 mongoose.Promise = Promise;
 
 import '/model/homework';
