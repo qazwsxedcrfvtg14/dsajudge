89d2cc80b9701d71c76428b22ed3c00e178a505b
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-09-27 15:29:40 +0800

    allow to run futex

--- isolate/isolate.c
+++ isolate/isolate.c
@@ -640,7 +640,7 @@ static void setup_seccomp(char **args)
                                   SCMP_SYS(close), SCMP_SYS(readlink),
                                   SCMP_SYS(sysinfo), SCMP_SYS(write),
                                   SCMP_SYS(writev), SCMP_SYS(lseek),
-                                  SCMP_SYS(clock_gettime)};
+                                  SCMP_SYS(clock_gettime),SCMP_SYS(futex)};
 
   int syscalls_whitelist_length = sizeof(syscalls_whitelist) / sizeof(int);
   
