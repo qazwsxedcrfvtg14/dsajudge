2d6e647819a9b2e39c5db67ebf68a1440680a96f
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2018-09-27 10:02:32 +0800

    try to add seccomp in judge

--- isolate/isolate.c
+++ isolate/isolate.c
@@ -24,6 +24,7 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <seccomp.h>
 
 /* May not be defined in older glibc headers */
 #ifndef MS_PRIVATE
@@ -637,6 +638,43 @@ box_inside(char **args)
   setup_credentials();
   setup_fds();
   char **env = setup_environment();
+  int syscalls_whitelist[] = {SCMP_SYS(read), SCMP_SYS(fstat),
+                                  SCMP_SYS(mmap), SCMP_SYS(mprotect),
+                                  SCMP_SYS(munmap), SCMP_SYS(uname),
+                                  SCMP_SYS(arch_prctl), SCMP_SYS(brk),
+                                  SCMP_SYS(access), SCMP_SYS(exit_group),
+                                  SCMP_SYS(close), SCMP_SYS(readlink),
+                                  SCMP_SYS(sysinfo), SCMP_SYS(write),
+                                  SCMP_SYS(writev), SCMP_SYS(lseek)};
+
+  int syscalls_whitelist_length = sizeof(syscalls_whitelist) / sizeof(int);
+  scmp_filter_ctx ctx = NULL;
+  // load seccomp rules
+  ctx = seccomp_init(SCMP_ACT_KILL);
+  if (!ctx) {
+      die("LOAD_SECCOMP_FAILED");
+  }
+  for (int i = 0; i < syscalls_whitelist_length; i++) {
+      if (seccomp_rule_add(ctx, SCMP_ACT_ALLOW, syscalls_whitelist[i], 0) != 0) {
+      die("LOAD_SECCOMP_FAILED");
+      }
+  }
+  // add extra rule for execve
+  if (seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(execve), 1, SCMP_A0(SCMP_CMP_EQ, (scmp_datum_t)(args[0]))) != 0) {
+      die("LOAD_SECCOMP_FAILED");
+  }
+  // do not allow "w" and "rw"
+  if (seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_WRONLY | O_RDWR, 0)) != 0) {
+      die("LOAD_SECCOMP_FAILED");
+  }
+  if (seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(openat), 1, SCMP_CMP(2, SCMP_CMP_MASKED_EQ, O_WRONLY | O_RDWR, 0)) != 0) {
+      die("LOAD_SECCOMP_FAILED");
+  }
+  if (seccomp_load(ctx) != 0) {
+      die("LOAD_SECCOMP_FAILED");
+  }
+  seccomp_release(ctx);
+
 
   if (set_cwd && chdir(set_cwd))
     die("chdir: %m");
