d9015eee1c46ab60a087100254a5d95df2001ed1
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2019-09-26 15:20:00 +0800

    new judge

--- src/server/scripts/clean_student_user.js
+++ src/server/scripts/clean_student_user.js
@@ -3,11 +3,11 @@ import User from '/model/user';
 import bcrypt from 'bcrypt';
 import {promisify} from 'bluebird';
 (async () => {
-  const students = await User.find({role: ['student']});
+  const students = await User.find({roles: ['student']});
   // console.log(students[0]);
   for (const student of students) {
     student.role = [];
     console.log(student);
-    // await student.save();
+    await student.save();
   }
 })();
